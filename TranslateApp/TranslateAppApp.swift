419d0bb9e3c4efa4a1976a0bd799fb35bd399645
Bodgar Jair Espinosa Miranda <bodgar_jair@hotmail.com>
2023-10-05 19:18:13 -0600

    Initial Commit

---
+++ TranslateApp/TranslateAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TranslateAppApp.swift
+//  TranslateApp
+//
+//  Created by Bodgar Espinosa Miranda on 05/10/23.
+//
+
+import SwiftUI
+
+@main
+struct TranslateAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
